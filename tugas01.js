c3ade511e989638021cf58d3df4ac7dee7e63d9f
Muhammad Faisal <muh.faisal572@gmail.com>
2023-07-10 18:58:46 +0700

    menambahkan tugas no.1

---
+++ tugas01.js
@@ -0,0 +1,33 @@
+const cekHariKerja = (day) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const dateDay = ["senin", "selasa", "rabu", "kamis", "jumat"];
+      let cek = dateDay.find((item) => {
+        return item === day;
+      });
+      if (cek) {
+        resolve(cek);
+      } else {
+        reject(new Error("Hari ini bukan hari kerja"));
+      }
+    }, 3000);
+  });
+};
+
+async function processData(day) {
+  try {
+    console.log("Mulai : menggunakan try catch");
+    const result = await cekHariKerja(day);
+    console.log(result);
+  } catch (error) {
+    console.log(error.message);
+  }
+}
+
+processData("senin")
+  .then(() => {
+    console.log("proses selesai");
+  })
+  .catch((error) => {
+    console.log("terdapat sebuah kesalahan", error);
+  });
